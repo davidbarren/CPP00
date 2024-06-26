645ab9af2da927840feb4c4e3b0252217a70179d
David Barrenechea <dbarrene@Davids-MacBook-Pro.local>
2024-06-26 15:43:06 +0300

    Made initial commit for ex00 and part of ex01

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarrene <dbarrene@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 03:19:55 by dbarrene          #+#    #+#             */
+/*   Updated: 2024/06/26 04:30:03 by dbarrene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include <string>
+int	main(int argc, char **argv)
+{
+	std::string str;
+
+	if (argc == 1)
+		std::cout << " * LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; argv[i]; i++)
+	{
+		std::string str = argv[i];
+		for (int k = str.length(); k >= 0 ; k--)
+				str[k] = toupper(str[k]);
+		std::cout << str;
+	}
+	std::cout << std::endl;
+	return (0);
+}
---
+++ ex01/Phonebook.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarrene <dbarrene@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:41:17 by dbarrene          #+#    #+#             */
+/*   Updated: 2024/06/26 15:42:39 by dbarrene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+class PhoneBook
+{
+	PhoneBook()
+	{
+	}
+	void add(Contact contact)
+	private:
+	Contact contacts[8];
+};
